400493a5682e914cbe9cb4bada0f49acb6a5ff44
Aonozan <aonozan@gmail.com>
2017-01-10 20:28:07 +0100

    Message class needs to implement Serializable.

--- src/basic/Message.java
+++ src/basic/Message.java
@@ -1,9 +1,13 @@
 
 package basic;
+
+import java.io.Serializable;
+
 /**
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
-public class Message {
+public class Message implements Serializable{
+	private static final long serialVersionUID = -1417092197358128858L;
 	public static final int TYPE_SPEECH = 0;
 	public static final int TYPE_WHOSIN = 1;
 	public static final int TYPE_LOGOUT = 2;
@@ -26,6 +30,8 @@ public class Message {
 	public String getMessageContent() {
 		return messageContent;
 	}
+	
+	
 	
 }
 
