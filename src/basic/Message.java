cd90ca875b923c8de7c09b074cd4f785cdbd3a2e
Aonozan <aonozan@gmail.com>
2017-01-10 11:34:06 +0100

    Message object done.

--- src/basic/Message.java
+++ src/basic/Message.java
@@ -4,8 +4,28 @@ package basic;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Message {
+	public static final int TYPE_SPEECH = 0;
+	public static final int TYPE_WHOSIN = 1;
+	public static final int TYPE_LOGOUT = 2;
 	
+	private int messageType;
+	private String messageContent;
 	
+	public Message(String messageContent) {
+		this(0, messageContent);
+	}
+	public Message(int messageType, String messageContent) {
+		super();
+		this.messageType = messageType;
+		this.messageContent = messageContent;
+	}
+	
+	public int getMessageType() {
+		return messageType;
+	}
+	public String getMessageContent() {
+		return messageContent;
+	}
 	
 }
 
